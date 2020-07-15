3e5f1ce70b80d308a3de1d1675e1df39beeb848f
Dorl Neylon <65495639+dorlneylon@users.noreply.github.com>
2020-07-16 01:56:51 +0300

    small addition

--- app_config.py
+++ app_config.py
@@ -6,6 +6,7 @@ config.read('config.cfg')
 PORT = config.get("server", "port")
 DEBUG = config.get("main", "debug")
 HOST = config.get("server", "host")
+POSTS_LIMIT = config.get("other", "max-posts-available")
 SQLALCHEMY_TRACK_MODS = config.get("other", "sqlalchemy-track-modifications")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODS
